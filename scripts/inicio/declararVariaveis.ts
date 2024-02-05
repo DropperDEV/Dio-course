5ffc9e0c4e356af4af23fb40c40a14cf90384e61
dropperDEV <jamescosta4747@Gmail.com>
2024-02-04 23:43:29 -0300

    feat: add how to declare variables, and array

--- scripts/inicio/declararVariaveis.ts
+++ scripts/inicio/declararVariaveis.ts
@@ -3,3 +3,30 @@ let paalvra :string;
 let palavraOuNum: number | string;
 let qualquerCoisa:any;
 const x : number = 10;
+
+interface Pessoa{
+    idade: number;
+    nome: string;
+    profissao?: string;
+}
+
+const PessoaEmpregada:Pessoa = {
+    idade: 20,
+    nome: "Rogerin",
+    profissao: "Programador"
+}
+
+const PessoaDesempregada:Pessoa = {
+    idade: 18,
+    nome: "Ardenilson",
+}
+
+const arrayPessoa:Array<Pessoa> = [
+    PessoaDesempregada,
+    PessoaEmpregada
+]
+
+const arrayPessoa2:Pessoa[] = [
+    PessoaDesempregada,
+    PessoaEmpregada
+]
